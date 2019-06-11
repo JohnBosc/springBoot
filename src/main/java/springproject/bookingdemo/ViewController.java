6143c01f0bda58c467dbc091f94ae0c623c7ee77
JohnBosc <jnrmengue@gmail.com>
2019-06-11 03:36:06 +0100

    Using Thymeleaf as the view template

---
+++ src/main/java/springproject/bookingdemo/ViewController.java
@@ -0,0 +1,20 @@
+package springproject.bookingdemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping(value = "/")
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Jean Bosco");
+        model.addAttribute("mode", "production");
+
+        return "index";
+    }
+}
